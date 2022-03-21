1f67ca9afba1acbcc298bf2ce0d119f24ab82ab4
Magali Fabri <magali_90@live.nl>
2022-03-21 21:41:45 +0100

    Get db params from heroku env vars

--- php/connectToDb.php
+++ php/connectToDb.php
@@ -2,9 +2,12 @@
 
 function connectToDb()
 {
-    $host = 'ec2-99-81-177-233.eu-west-1.compute.amazonaws.com';
-    $dbname = 'd7e7acgoltcb73';
-    $username = 'hpaiicvwcjwvcs';
-    $password = '6b04237ffc00985813eea3cbefe0975bdbf852aad51469158c95976d5853e802';
-    return pg_connect("host=${host} dbname=${dbname} user=${username} password=${password}");
+    $dbParams = parse_url(getenv("DATABASE_URL"));
+
+    $host = $dbParams['host'];
+    $user = $dbParams['user'];
+    $password = $dbParams['pass'];
+    $dbname = ltrim($dbParams["path"], "/");
+
+    return pg_connect("host=${host} dbname=${dbname} user=${user} password=${password}");
 }
